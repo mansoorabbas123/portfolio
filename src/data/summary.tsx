30b92dae669558b68c4a4693aaa9f64216012223
mansoorabbas123 <mansoor.abbas651@gmail.com>
2024-03-29 12:16:31 -0400

    update summary data

--- src/data/summary.tsx
+++ src/data/summary.tsx
@@ -3,24 +3,37 @@ import { v4 as uuid } from "uuid";
 export const summery = {
   jsx: [
     <p key={uuid()}>
-      Back in 2012, I decided to try my hand at creating custom Tumblr themes
+      {/* Back in 2012, I decided to try my hand at creating custom Tumblr themes
       and tumbled head first into the rabbit hole of coding and web development.
       Fast-forward to today, and I have had the privilege of building software
       for an <b>advertising agency</b>, a <b>start-up</b>, a
-      <b>student-led design studio</b>, and a <b>huge corporation</b>.
+      <b>student-led design studio</b>, and a <b>huge corporation</b>. */}
+      Motivated self-learner with excelent experience, passion for innovation and delivering top-quality
+      solutions. Proficient in building <b>scalable web
+        applications</b>, <b>integrating features</b>, and
+      maintaining <b>complex codebases</b> to meet <b>client
+        expectations</b> and <b>organizational objectives</b>.
+
     </p>,
     <p key={uuid()}>
-      My main focus these days is building products and leading projects for our
+      {/* My main focus these days is building products and leading projects for our
       clients at Upstatement. I most enjoy building software in the sweet spot
       where design and engineering meet — things that look good but are also
       built well under the hood. In my free time, I have also released an online
       video course that covers everything you need to know to build a web app
-      with the Spotify API.
+      with the Spotify API. */}
+      👋 Welcome to my corner of the digital universe! I'm a professional JavaScript developer, and I'm on a mission to create dynamic, interactive web applications that truly make a difference. My journey in the world of technology has taken me through a captivating landscape of tools and frameworks, and I've had the pleasure of mastering many.
     </p>,
     <p key={uuid()}>
-      When I am not at the computer, I am usually rock climbing, reading,
+      {/* When I am not at the computer, I am usually rock climbing, reading,
       hanging out with my wife and two cats, or running around Hyrule searching
-      for{" "}
+      for{" "} */}
+      With these versatile instruments, I craft scalable, high-performance, and user-friendly solutions across a wide range of domains and challenges.
     </p>,
+    <p key={uuid()}>
+      🚀 Let's Collaborate:
+      I thrive on new opportunities and collaborative ventures that help me grow as both a developer and a person. Whether you're interested in working with me or just want to have a friendly chat, don't hesitate to reach out. You can contact me via LinkedIn or email.
+
+    </p>
   ],
 };
